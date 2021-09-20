cb55d600840a616d8339d396f239f59c9d5ff942
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-09-20 14:57:15 +0200

    feat: Initial commit. Architecture. Basic styling. Main layout

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cryptodetails = () => {
+    return (
+        <div>
+            <h1>CryptoDetails</h1>
+        </div>
+    )
+}
+
+export default Cryptodetails;
