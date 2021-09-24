07fdb8aa56aa104b7a552db248444623d28ed959
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-09-24 12:54:14 +0200

    feat: Add XRyptocurrencies cards

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import millify from 'millify';
+import { Card, Row, Col, Input } from 'antd';
+
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const Cryptocurrencies = () => {
+    const { data: cryptosList, isFetching } = useGetCryptosQuery();
+    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+
+    console.log(cryptos);
+
+
     return (
-        <div>
-            <h1>Cryptocurrencies</h1>
-        </div>
+        <>
+            <Row gutter={[32, 32]} className="crypto-card-container">
+                {cryptos?.map((cryptoCurrency) => (
+                    <Col key={cryptoCurrency.id} xs={24} sm={12} lg={6} className="crypto-card" span={12} >
+                        <Link to={`/crypto/${cryptoCurrency.id}`}>
+                            <Card
+                                title={`${cryptoCurrency.rank}, ${cryptoCurrency.name}`}
+                                extra={<img className="crypto-image" src={cryptoCurrency.iconUrl} alt={cryptoCurrency.name} />}
+                                hoverable
+                            >
+                                <p>Price: {millify(cryptoCurrency.price)}</p>
+                                <p>Market Cap: {millify(cryptoCurrency.marketCap)}</p>
+                                <p>Daily Change: {millify(cryptoCurrency.change)}</p>
+                            </Card>
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
 
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 
-import Loader from './Loader'
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Card, Row, Col, Input } from 'antd';
+import Loader from './Loader';
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 import { useGetCryptosQuery } from '../services/cryptoApi';
-import { Loading3QuartersOutlined } from '@ant-design/icons';
 
 const News = ({ simplified }) => {
     const [newsCathegory, setNewsCathegory] = useState('Cryptocurrencies');
     const { data } = useGetCryptoNewsQuery(100);
     const { data: cryptoNews } = useGetCryptosQuery({ newsCathegory, count: simplified ? 6 : 12 });
 
-if(!cryptoNews) return <Loader />
+    if (!cryptoNews) return <Loader />
 
     return (
         <div>
